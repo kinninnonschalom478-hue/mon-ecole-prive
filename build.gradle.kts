e17e77521c0c2e4ca7c2c4f642a24d4bee768d50
SCHAGLK <schaglk@localhost>
2026-09-19 00:45:14 +0100

    fix: align Java and Kotlin JVM targets

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,17 @@ plugins {
     id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
+
+
+allprojects {
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+        }
+    }
+
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
+    }
+}
